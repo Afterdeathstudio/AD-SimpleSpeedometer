b5df8d2100ba6d128805535b75e292afa1f5bca3
TenTypeeek <158502569+TenTypeeek@users.noreply.github.com>
2025-06-19 15:34:51 +0200

    Update config.lua

--- ad-simplespeedometer/config.lua
+++ ad-simplespeedometer/config.lua
@@ -1,3 +1,10 @@
+--      _     __ _            ____             _   _     
+--     / \   / _| |_ ___ _ __|  _ \  ___  __ _| |_| |__  
+--    / _ \ | |_| __/ _ \ '__| | | |/ _ \/ _` | __| '_ \ 
+--   / ___ \|  _| ||  __/ |  | |_| |  __/ (_| | |_| | | |
+--  /_/   \_\_|  \__\___|_|  |____/ \___|\__,_|\__|_| |_|
+-- https://discord.gg/5D3wdy4dQH ┃ https://github.com/Afterdeathstudio ┃ https://afterdeath.gitbook.io/afterdeath-studios/
+
 Config = {}
 
 -- Units: 'KPH' or 'MPH'
